6fe5e9623c696e721429a48cb088b922b4bdeece
Рахим <114479886+Rahim-Kadyrov@users.noreply.github.com>
2022-10-29 13:11:51 +0300

    Add files via upload

---
+++ frontend9-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+    <div className='App'>
+        <h1>Hello</h1>
+        <select>
+            <option value="1">One</option>
+            <option value="1">Two</option>
+            <option value="1">Theree</option>
+        </select>
+        <hr/>
+        <img src="/logo192.png"/>
+    </div>
+    );
+}
+
+export default App;
